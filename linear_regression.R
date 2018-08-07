5aef811ad323ba6cc7b39e8ba5080fcfc66289e2
GYuWang <guangywang@gmail.com>
2018-08-07 10:58:57 -0500

    first commit

---
+++ linear_regression.R
@@ -0,0 +1,15 @@
+IMR_mtr = read.table('/Users/guangyu/Work/Hi-C/Data/Contactmatrix/IRM90/matrix/5_matrix_IMR90_Coverage.txt.100.1500',header = F)
+IMR_band = read.table('/Users/guangyu/Work/Hi-C/Data/Contactmatrix/IRM90/matrix/5_matrix_IMR90_Coverage.txt.100.1500.band.txt')
+NHEK_mtr = read.table('/Users/guangyu/Work/Hi-C/Data/Contactmatrix/NHEK/5_matrix_NHEK_Coverage.txt.100.1500',header = F)
+NHEK_band = read.table('/Users/guangyu/Work/Hi-C/Data/Contactmatrix/NHEK/5_matrix_NHEK_Coverage.txt.100.1500.band.txt')
+GM12878_mtr = read.table('/Users/guangyu/Work/Hi-C/Data/Contactmatrix/GM12878/5_matrix_GM12878_Coverage.txt.100.1500',header = F)
+GM12878_band = read.table('/Users/guangyu/Work/Hi-C/Data/Contactmatrix/GM12878/5_matrix_GM12878_Coverage.txt.100.1500.band.txt')
+HUVEC_mtr = read.table('/Users/guangyu/Work/Hi-C/Data/Contactmatrix/HUVEC/5_matrix_HUVEC_Coverage.txt.100.1500',header = F)
+HUVEC_band = read.table('/Users/guangyu/Work/Hi-C/Data/Contactmatrix/HUVEC/5_matrix_HUVEC_Coverage.txt.100.1500.band.txt')
+HMEC_mtr = read.table('/Users/guangyu/Work/Hi-C/Data/Contactmatrix/HMEC/optimaze/5_matrix_HMEC_Coverage.txt.8000.9000',header = F)
+HMEC_band = read.table('/Users/guangyu/Work/Hi-C/Data/Contactmatrix/HMEC/optimaze/5_matrix_HMEC_Coverage.txt.8000.9000.band.txt')
+
+print(HMEC_band)
+
+
+
